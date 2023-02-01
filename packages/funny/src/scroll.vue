b6cb232a61f3f0a4216c5749d93df08de542118a
zhanggaodu <1030314249@qq.com>
2023-02-01 11:32:33 +0800

    scroll

---
+++ packages/funny/src/scroll.vue
@@ -0,0 +1,27 @@
+<script setup >
+const io = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    // Add 'active' class if observation target is inside viewport
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('active');
+    }
+    // Remove 'active' class otherwise
+    else {
+      entry.target.classList.remove('active');
+    }
+  })
+})
+
+// Declares what to observe, and observes its properties.
+const boxElList = document.querySelectorAll('.box');
+boxElList.forEach((el) => {
+  io.observe(el);
+})
+</script>
+<template>
+滚动监听
+
+</template>
+<style lang="scss" scoped>
+
+</style>
