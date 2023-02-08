e9f118ea5ea6ebdf15d489e7032b793e8bf5391d
zhanggaodu <1030314249@qq.com>
2023-02-08 09:46:12 +0800

    typing

---
+++ packages/funny/src/typing.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <div class="typing-demo">
+      This is a typing demo.
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  /*This part is important for centering*/
+  display: grid;
+  place-items: center;
+}
+
+.typing-demo {
+  width: 22ch;
+  animation: typing 2s steps(22), blink .5s step-end infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  font-family: monospace;
+  font-size: 2em;
+}
+
+@keyframes typing {
+  from {
+    width: 0
+  }
+}
+    
+@keyframes blink {
+  50% {
+    border-color: transparent
+  }
+}
+</style>
