eb47a7b690045592375c3f1051e8bcc53607fa09
zhanggaodu <1030314249@qq.com>
2023-02-17 22:16:43 +0800

    copy

---
+++ packages/funny/src/clipboard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+/**
+ * 思路 
+ * 接受两个参数 text 和 container
+ * 判断是否支持command  navigate.clipboard(https)
+ * 为什么创建textarea
+ * 有点简单 可以写个库放在npm上惹
+*/
+const copyUseCommand = (text: string) => {
+
+}
+const copyUseNavigate = (text: string) => {
+
+}
+
+const copy = (text: any, container: HTMLElement) => {
+  // text接受的类型 string htmlElement
+
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped >
+
+</style>
