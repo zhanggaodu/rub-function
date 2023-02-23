861bbcfd705190f4191c74c055e9ee1bec96c511
zhanggaodu <1030314249@qq.com>
2023-02-23 21:10:01 +0800

    child

---
+++ packages/funny/src/components/child.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GrandChild from './grandChild.vue'
+</script>
+<template>
+  子组件
+  emit
+  VUE2 eventBus
+  VUE3 mitt 、tiny-emitter
+  provide / inject API
+  $attrs / $listeners
+  Vuex
+  v-module 自定义组件 双向绑定
+  <GrandChild />
+
+</template>
+<style lang="scss" scoped >
+
+</style>
