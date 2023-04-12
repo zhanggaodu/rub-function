782821f61130eef5543ec4befd2ddd82febaeae2
zhanggaodu <1030314249@qq.com>
2023-04-12 21:46:12 +0800

    calculate

---
+++ packages/funny/src/calculate.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const calculate = (num: number) => {
+  // 处理正负小数
+  // return Math.fround(num).toFixed(1)
+  // 计算带小数的数字 1.1+2.2
+}
+</script>
+<template>
+
+
+</template>
+<style lang="scss" scoped>
+
+</style>
