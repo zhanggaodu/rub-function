23d18b17de21a96dba4f60640d686ca29292399f
zhanggaodu <1030314249@qq.com>
2024-01-09 10:19:01 +0800

    d

--- packages/modes/vite.config.ts
+++ packages/modes/vite.config.ts
@@ -1,4 +1,3 @@
-import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 import path, { resolve } from 'path'
@@ -7,49 +6,8 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 const env = 'dev'
-const rewriteDefaultConfig = {
-  changeOrigin: true,
-  // target: `https://art-my-${env}.windimg.com/`,
-  target: `http://10.129.152.57:8080/`,
-  ws: true,
-  headers: {
-    referer: 'windimg.com'
-  },
 
-  logLevel: 'debug',
-  configure: proxy => {
-    proxy.on('proxyReq', (req, a) => {
-      req.removeHeader('Connection') // npm node-http-proxy
-    })
-  },
-  onProxyRes: proxyResponse => {
-    if (proxyResponse.headers['set-cookie']) {
-      const cookies = proxyResponse.headers['set-cookie'].map(cookie =>
-        cookie.replace(/; secure/gi, '')
-      );
-      proxyResponse.headers['set-cookie'] = cookies;
-    }
-  }
-}
-
-const proxy = {
-  '/api': rewriteDefaultConfig,
-  '/passport': rewriteDefaultConfig,
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/rush_assets_cdnhost/modes/',
-  // build: {
-  //   target: 'es2018',
-  //   outDir: '../../dist/modes',
-  //   rollupOptions: {
-  //     input: {
-  //       index: resolve(__dirname, 'index.html'),
-  //       // responsibility: resolve(__dirname, 'index.html'),
-  //     }
-  //   }
-  // },
   plugins: [
     vue(),
     Components({
@@ -57,9 +15,6 @@ export default defineConfig({
       dirs: [
         'src/components',
       ]
-    }),
-    legacy({
-      targets: ['defaults', 'not IE 11']
     })
   ],
   resolve: {
@@ -77,6 +32,5 @@ export default defineConfig({
   },
   server: {
     port: 1993,
-    proxy
   },
 })
