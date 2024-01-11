d3f7820d2e65c07ec67972dba2d15c356d57239f
zhanggaodu <1030314249@qq.com>
2024-01-11 15:50:55 +0800

    currying

--- packages/modes/src/Main.vue
+++ packages/modes/src/Main.vue
@@ -46,8 +46,35 @@ const str = ''
     console.log( obj.getName() ) // 输出：'sven'
     var getName2 = obj.getName  undefined
     call是apply的语法糖，如果需要明确传入参数的次数可以使用call，一般情况下使用apply一次性传入所有的参数。
-    59
- * 
+    闭包不会造成内存泄露，存放在闭包的变量和放在全局环境是一样的
+ * currying 又称部分求值。一个 currying 的函数首先会接受一些参数，接受了这些参数之后，该函数并不会立即求值，而是继续返回另外
+ 一个函数，刚才传入的参数在函数形成的闭包中被保存起来。待到函数被真正需要求值的时候，之前传入的所有参数都会被一次性用于求值。
+ *var currying = function( fn ){
+  var args = [];
+  return function(){
+    if ( arguments.length === 0 ){
+      return fn.apply( this, args );
+    } else{
+      [].push.apply( args, arguments );
+      return arguments.callee;
+      }
+    }
+  };
+  var cost = (function(){
+    var money = 0;
+    return function(){
+      for ( var i = 0, l = arguments.length; i < l; i++ ){
+      money += arguments[ i ];
+    }
+    return money;
+    }
+  })();
+  var cost = currying( cost ); // 转化成 currying 函数
+  cost( 100 ); // 未真正求值
+  cost( 200 ); // 未真正求值
+  cost( 300 ); // 未真正求值
+  alert ( cost() ); // 求值并输出：600
+ * 72
  */
 
 
